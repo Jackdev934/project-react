707889ee9a6e2aca2e11cdfbf653396d3b252349
Jackdev934 <Jackdev934@gmail.com>
2025-11-13 12:46:25 -0500

    browser router

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -46,7 +46,7 @@ export default function App() {
       : "/";
 
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename="https://jackdev934.github.io/project-react">
       <Routes>
         <Route path="/*" element={<Layout />} />
       </Routes>
