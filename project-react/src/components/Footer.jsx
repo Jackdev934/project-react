c55d76afd961135d21c5753c49237173aadb7e65
= <jackdev934@gmail.com>
2025-11-30 14:42:17 -0500

    render link on website

--- project-react/src/components/Footer.jsx
+++ project-react/src/components/Footer.jsx
@@ -1,8 +1,23 @@
+import "../css/Footer.css";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
         <p>&copy; {new Date().getFullYear()} Astora’s Archive • All rights reserved.</p>
+
+        {/* Added backend link for assignment */}
+        <p className="backend-link">
+          Backend API hosted on Render:{" "}
+          <a
+            href="https://project-backend-fl7h.onrender.com"
+            target="_blank"
+            rel="noreferrer"
+            className="render-anchor"
+          >
+            https://project-backend-fl7h.onrender.com
+          </a>
+        </p>
       </div>
     </footer>
   );
