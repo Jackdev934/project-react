a2debfb05843a00c774ca93a6018e938cbc90ed4
= <jackdev934@gmail.com>
2025-12-05 19:02:32 -0500

    p

--- project-react/src/config.js
+++ project-react/src/config.js
@@ -1,11 +1,10 @@
 // src/config.js
 
-// 👇 change this to your REAL Render URL
 const PROD_BACKEND_URL = "https://project-backend-fl7h.onrender.com";
 
 const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? PROD_BACKEND_URL         // when built & deployed
-    : "http://localhost:3001"; // when running npm start locally
+    ? PROD_BACKEND_URL
+    : "http://localhost:3001";
 
 export default BACKEND_URL;
